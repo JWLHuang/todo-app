4638aecfa81948fb43ef20b305635e427264edbd
Jacky Huang <jacky.h1218@gmail.com>
2023-11-11 17:23:36 -0800

    Add mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -58,15 +58,21 @@ router.delete('/:id', async (req: Request, res: Response) => {
 // app
 const port = 3000;
 const app = express();
-
 app.use(express.json());
 
-app.use('/api/todoList', router);
+CrudService.connectToDatabase()
+    .then(() => {
+        app.use('/api/todoList', router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+        app.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
 
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
+        app.listen(port, () => {
+            return console.log(`Express is listening at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error("Database connection failed", error);
+        process.exit();
+    });
--- src/crud/service.ts
+++ src/crud/service.ts
@@ -1,7 +1,24 @@
 import { todoList } from "../data/list";
 import { CATEGORY, TodoItem } from "../model/todoItem";
+import * as mongoDB from "mongodb";
+import * as dotenv from "dotenv";
 
 export const getTodoList = async (): Promise<TodoItem[]> => Object.values(todoList);
+export const collections: { todoList?: mongoDB.Collection } = {}
+
+export async function connectToDatabase () {
+   dotenv.config();
+
+   const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+
+   await client.connect();
+
+   const db: mongoDB.Db = client.db(process.env.DB_NAME);
+   const todoList: mongoDB.Collection = db.collection(process.env.COLLECTION_NAME);
+
+    collections.todoList = todoList;
+    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${todoList.collectionName}`);
+}
 
 export const addTodoItem = async (title: string, category: CATEGORY, description: string): Promise<TodoItem> => {
     const newID: number = Object.keys(todoList).length + 1;
