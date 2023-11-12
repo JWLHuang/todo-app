3d999d4722d0dc6e7d3ca1322d5162398df4179c
Jacky Huang <jacky.h1218@gmail.com>
2023-11-12 01:44:41 -0800

    Update delete from mongodb

--- src/router.ts
+++ src/router.ts
@@ -42,7 +42,7 @@ router.post('/new', async (req: Request, res: Response) => {
 
 router.put('/:id', async (req: Request, res: Response) => {
     try {
-        const id = req.params.id;
+        const id: string = req.params.id;
         const query = { _id: new ObjectId(id) };
         const infoToUpdate: TodoItem = {...req.body};
         const updatedItem = await CrudService.updateTodoItem(query, infoToUpdate);
@@ -56,10 +56,23 @@ router.put('/:id', async (req: Request, res: Response) => {
 
 router.delete('/:id', async (req: Request, res: Response) => {
     try {
-    const idToDelete: number = parseInt(req.params.id);
-    await CrudService.deleteTodoItem(idToDelete);
-    res.sendStatus(204);
-    } catch (e) {
-        res.status(500).send(e.message);
+    const id: string = req.params.id;
+    const idToDelete = { _id: new ObjectId(id)};
+    // await CrudService.deleteTodoItem(idToDelete);
+    const removedItem = await CrudService.collections.todoList.deleteOne(idToDelete);
+    if (removedItem && removedItem.deletedCount) {
+            res.status(202).send(`Successfully removed todoItem with id ${id}`);
+        } else if (!removedItem) {
+            res.status(400).send(`Failed to remove todoItem with id ${id}`);
+        } else if (!removedItem.deletedCount) {
+            res.status(404).send(`todoItem with id ${id} does not exist`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
     }
+    // res.sendStatus(204);
+    // } catch (e) {
+    //     res.status(500).send(e.message);
+    // }
 });
