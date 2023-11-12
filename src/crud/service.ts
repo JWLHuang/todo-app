5b27d4d07dadc7a098a26c73df08b7ccaa21820a
Jacky Huang <jacky.h1218@gmail.com>
2023-11-12 02:37:59 -0800

    Remove commented out and unused code

--- src/crud/service.ts
+++ src/crud/service.ts
@@ -1,11 +1,8 @@
-import { todoList } from "../data/list";
 import { CATEGORY, TodoItem } from "../model/todoItem";
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
 
-// export const getTodoList = async (): Promise<TodoItem[]> => Object.values(todoList);
 export const collections: { todoList?: mongoDB.Collection } = {}
-
 
 export const connectToDatabase = async () => {
     dotenv.config();
@@ -32,7 +29,6 @@ export const findTodoItem = async (_id: mongoDB.Filter<mongoDB.BSON.Document>): 
 }
 
 export const addTodoItem = async (title: string, category: CATEGORY, description: string): Promise<TodoItem> => {
-    // const newID: number = Object.keys(todoList).length + 1;
     const currentDate: number = Date.now();
     const newTodoItem: TodoItem = {
         title,
@@ -44,13 +40,8 @@ export const addTodoItem = async (title: string, category: CATEGORY, description
         _id: null,
     };
     await collections.todoList.insertOne(newTodoItem);
-
-    // todoList[newID] = newTodoItem;
-    // console.log("Added" + newTodoItem);
     return newTodoItem;
 }
-
-// export const findTodoItem = async (id: number): Promise<TodoItem> => todoList[id];
 
 export const updateTodoItem = async (id: mongoDB.Filter<mongoDB.BSON.Document>, updatedItem: TodoItem): Promise<TodoItem> => {
     const toUpdate: TodoItem = await collections.todoList.findOne(id);
@@ -61,13 +52,10 @@ export const updateTodoItem = async (id: mongoDB.Filter<mongoDB.BSON.Document>, 
 }
 
 export const deleteTodoItem = async (id: mongoDB.Filter<mongoDB.BSON.Document>): Promise<mongoDB.DeleteResult> => {
-    // const itemToDelete: TodoItem = await findTodoItem(id);
     const itemToDelete = await collections.todoList.deleteOne(id);
     if (!itemToDelete) {
         return null;
     }
-    
     console.log("Deleted" + id);
     return itemToDelete;
-    // delete todoList[id];
 }
--- src/router.ts
+++ src/router.ts
@@ -8,8 +8,6 @@ router.use(express.json())
 
 router.get('/', async (req: Request, res: Response) => {
     try {
-        // const todoList: TodoItem[] = await CrudService.getTodoList();
-        // const todoList = await CrudService.collections.todoList.find({}).toArray();
         const todoList = await CrudService.getTodoList();
         res.status(200).send(todoList);
     } catch (e) {
@@ -21,9 +19,6 @@ router.get('/:id' , async (req: Request, res: Response) => {
     try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
-        // const query = {"_id" : idNumber} 
-        // const foundTodoItem = await CrudService.findTodoItem(idNumber);
-        // const foundTodoItem = await CrudService.collections.todoList.findOne(query);
         const foundTodoItem = await CrudService.findTodoItem(query);
         res.status(200).send(foundTodoItem);
     } catch (e) {
@@ -35,7 +30,6 @@ router.post('/new', async (req: Request, res: Response) => {
     try {
         const {title, category, description} = req.body;
         const TodoItemToAdd = await CrudService.addTodoItem(title, category, description);
-        // const newToDoItem = await CrudService.collections.todoList.insertOne(TodoItemToAdd);
         res.status(201).json(TodoItemToAdd);
     } catch (e) {
         res.status(500).send(e.message);
@@ -48,7 +42,6 @@ router.put('/:id', async (req: Request, res: Response) => {
         const query = { _id: new ObjectId(id) };
         const infoToUpdate: TodoItem = {...req.body};
         const updatedItem = await CrudService.updateTodoItem(query, infoToUpdate);
-        // const updateDataBase = await CrudService.collections.todoList.updateOne(query, { $set: updatedItem});
         res.status(200).json(updatedItem);
     } catch (e) {
         res.status(500).send(e.message);
@@ -59,8 +52,6 @@ router.delete('/:id', async (req: Request, res: Response) => {
     try {
         const id: string = req.params.id;
         const idToDelete = { _id: new ObjectId(id)};
-        // await CrudService.deleteTodoItem(idToDelete);
-        // const removedItem = await CrudService.collections.todoList.deleteOne(idToDelete);
         const removedItem = await CrudService.deleteTodoItem(idToDelete);
         if (removedItem && removedItem.deletedCount) {
                 res.status(202).send(`Successfully removed todoItem with id ${id}`);
@@ -73,8 +64,4 @@ router.delete('/:id', async (req: Request, res: Response) => {
         console.error(error.message);
         res.status(400).send(error.message);
     }
-    // res.sendStatus(204);
-    // } catch (e) {
-    //     res.status(500).send(e.message);
-    // }
 });
