11cf6f1e61e114540734a2d3b3af7453beace07f
Jacky Huang <jacky.h1218@gmail.com>
2023-11-11 17:31:41 -0800

    Move router logic into its own module

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,6 @@
-import express, { Request, Response } from 'express';
-import { TodoItem } from './model/todoItem';
+import express from 'express';
 import * as CrudService from './crud/service';
-
-// api router - TODO move this to its own module
-const router = express.Router();
-
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const todoList: TodoItem[] = await CrudService.getTodoList();
-        res.status(200).send(todoList);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-router.get('/:id' , async (req: Request, res: Response) => {
-    try {
-    const idNumber: number = parseInt(req.params.id);
-    const foundTodoItem = await CrudService.findTodoItem(idNumber);
-    res.status(200).send(foundTodoItem);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-router.post('/new', async (req: Request, res: Response) => {
-    try {
-        const {title, category, description} = req.body;
-        const newTodoItem = await CrudService.addTodoItem(title, category, description);
-        res.status(201).json(newTodoItem);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-router.put('/:id', async (req: Request, res: Response) => {
-    try {
-    const idToUpdate: number = parseInt(req.params.id);
-    const infoToUpdate: TodoItem = {id: idToUpdate, ...req.body};
-    const updatedItem = await CrudService.updateTodoItem(idToUpdate, infoToUpdate);
-    res.status(200).json(updatedItem);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
-
-router.delete('/:id', async (req: Request, res: Response) => {
-    try {
-    const idToDelete: number = parseInt(req.params.id);
-    await CrudService.deleteTodoItem(idToDelete);
-    res.sendStatus(204);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
-});
+import { router } from './router';
 
 // app
 const port = 3000;
---
+++ src/router.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import { TodoItem } from "./model/todoItem";
+import * as CrudService from "./crud/service";
+
+export const router = express.Router();
+router.use(express.json())
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const todoList: TodoItem[] = await CrudService.getTodoList();
+        res.status(200).send(todoList);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/:id' , async (req: Request, res: Response) => {
+    try {
+    const idNumber: number = parseInt(req.params.id);
+    const foundTodoItem = await CrudService.findTodoItem(idNumber);
+    res.status(200).send(foundTodoItem);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.post('/new', async (req: Request, res: Response) => {
+    try {
+        const {title, category, description} = req.body;
+        const newTodoItem = await CrudService.addTodoItem(title, category, description);
+        res.status(201).json(newTodoItem);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.put('/:id', async (req: Request, res: Response) => {
+    try {
+    const idToUpdate: number = parseInt(req.params.id);
+    const infoToUpdate: TodoItem = {id: idToUpdate, ...req.body};
+    const updatedItem = await CrudService.updateTodoItem(idToUpdate, infoToUpdate);
+    res.status(200).json(updatedItem);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+    const idToDelete: number = parseInt(req.params.id);
+    await CrudService.deleteTodoItem(idToDelete);
+    res.sendStatus(204);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
